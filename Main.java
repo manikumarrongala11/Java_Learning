b9532de4cd9bebf1bcd593adf2daba8d943a6ef3
Rongala Kuladeep Manikumar <manikumarrongala11@gmail.com>
2026-06-30 12:25:53 +0530

    Initial Java Practice

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a[]={10,20,30};
+        int b[]=a;
+        b[0]=1110;
+        System.out.println(a[0]);
+
+        
+    }
+}
