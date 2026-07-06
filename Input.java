87321858b481d2a993764b27d873cafeecb7a82f
Rongala Kuladeep Manikumar <manikumarrongala11@gmail.com>
2026-07-06 12:56:38 +0530

    Added input

---
+++ Input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Input{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("naam kya hai tumra");
+            String name = scanner.nextLine();
+            System.out.print(name + " acha ");
+             scanner.close();
+
+        
+
+    }
+}
