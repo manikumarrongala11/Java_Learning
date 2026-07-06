c81d603217b8bf43cb653d1225254cd26c5f1ec8
Rongala Kuladeep Manikumar <manikumarrongala11@gmail.com>
2026-07-06 12:20:31 +0530

    Removed Students.java

--- Variable.java
+++ Variable.java
@@ -1,2 +1,59 @@
-//here the date is 6 th of july day 1
-today we 
+public class Variable {
+
+     public static void main(String[] args){
+
+        //today is 6th of june now we are going to learn java
+        //today is the day 1 
+        //codeeater 
+        //A variable is a container (or box) that stores a value so your program can use it later. 
+        // premitive = simple value directly stored in the  memory (stack)
+        //reference = memory address (stack) that points to the (heap)
+
+        // Primitive va Reference
+
+        // int
+        // double
+        // float 
+        // Char
+        // Boolean
+
+
+
+
+        // 2 steps to create variable
+        // 1. Declaration
+        // 2. Assignment
+        int age = 30;
+        int score = 300;
+
+        double cost = 34.54;
+        double distance = 2.5;
+
+        float  value = 2.55f;
+
+        char Grade = 'o';
+        
+        boolean isStudent = false;
+        boolean forSale = true;
+        boolean youCome = false;
+
+
+        String name = "mani";
+        String height = "6 feet";
+        String College = "LPU";
+        if (isStudent){
+            System.out.println(name);
+        }
+        else{
+            System.out.println(height + " mandratorty in army ");
+        }
+
+        //System.out.println(name + " is a codeeater");
+
+
+    }
+
+
+
+
+}
