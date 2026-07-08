a1707f1f998bf7a269e0c4e15c9d98ead7572fdf
Rongala Kuladeep Manikumar <manikumarrongala11@gmail.com>
2026-07-08 23:19:05 +0530

    New Changes

---
+++ MathClass.java
@@ -0,0 +1,24 @@
+public class MathClass {
+    public static void main(String[] args){
+        //System.out.println(Math.PI);
+        //System.out.println(Math.E);
+
+        double result;
+
+        result = Math.pow(2,5);
+        result = Math.pow(2,4);
+        result = Math.abs(-5);
+        result = Math.sqrt(9);
+        result = Math.round(3.14);
+        result = Math.ceil(3.14);
+        result = Math.floor(3.14);
+        result = Math.max(12, 23);
+        result = Math.min(23, 56);
+        
+        
+
+        System.out.println(result);
+
+
+    }
+}
