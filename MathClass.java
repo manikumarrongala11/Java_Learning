cbf90a87c5b54c317ec9917a47ea09764d7dba04
Rongala Kuladeep Manikumar <manikumarrongala11@gmail.com>
2026-07-13 11:53:46 +0530

    Added MathClass and Sum

--- MathClass.java
+++ MathClass.java
@@ -1,4 +1,4 @@
-public class MathClass {
+/*public class MathClass {
     public static void main(String[] args){
         //System.out.println(Math.PI);
         //System.out.println(Math.E);
@@ -18,6 +18,36 @@ public class MathClass {
         
 
         System.out.println(result);
+
+
+    }
+}
+*/
+import java.util.Scanner;
+public class MathClass{
+    public static void main(String[] args){
+        //HYPOTENUSE c = math.sqrt(a² + b²)
+        Scanner sc = new Scanner(System.in);
+
+        double a;
+        double b;
+        double c;
+
+        System.out.print("Enter side a: ");
+        a = sc.nextDouble();
+
+        System.out.print("Enter side B : ");
+        b = sc.nextDouble();
+
+        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+        System.out.println("the Hypotenuse is (side c) " + c + "cm");
+
+
+
+
+
+        sc.close();
 
 
     }
