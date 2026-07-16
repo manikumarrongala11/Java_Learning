abc864b7d1b2f482a41e45d2a5b22a3bd7f439fe
Rongala Kuladeep Manikumar <manikumarrongala11@gmail.com>
2026-07-16 23:20:51 +0530

    added practice in Loops

--- Loops.java
+++ Loops.java
@@ -1,7 +1,57 @@
 public class Loops{
     public static void main(String[] args){
 
-        
 
+       
+           /*for Loop
+       while Loop
+       do while Loop
+       for(initialisation; condition; updation){
+       
+       //do something
+       }
+       System..
+
+       
+       | Code     | Meaning       |
+| -------- | ------------- |
+| `i++`    | Add 1         |
+| `i--`    | Subtract 1    |
+| `i += 2` | Add 2         |
+| `i -= 2` | Subtract 2    |
+| `i *= 2` | Multiply by 2 |
+| `i /= 2` | Divide by 2   |
+
+      counter++ = counter = counter + 1
+       for(int counter = 0; counter < 11 ; counter++){
+    /System.out.print( counter + "  " ); */
+    /*  while Loop
+    while(condition){
+        do something
     }
-}
+   
+*/
+
+
+        int i = 1;
+
+        while (i <= 50) {
+
+            if (i % 5 == 0) {
+                System.out.println("Multiple of 5: " + i);
+            } else {
+                System.out.println(i);
+            }
+
+            i++;
+        }
+
+    
+
+ 
+    }
+      }
+
+        
+    
+    
---
+++ LoopsPractice.java
@@ -0,0 +1,35 @@
+public class LoopsPractice {
+    public static void main(String[] args){
+      /*  // find even numbers between 1 and 20
+        //for(int i =20; i>=2; i-=2){
+           // System.out.println(i);
+       // }
+       
+       // print the 7th table till 70 using for loop
+       //7 * 1 = 7;
+       //7 * 2 = 14;
+    //for(int i = 1; i <= 10; i++){
+      //  System.out.println("7 * " + i + " = " + 7 * i);
+   // }
+   
+
+   //for(int p = 50; p<=100; p++)
+   //System.out.println("NUM: " + p);
+
+   //for(int i = 100; i>=50; i--){
+      //System.out.println(i);
+   //}
+   // 5 * 1 = 5
+   //for(int i = 1; i<=20; i++)
+   //System.out.println(i * 5);
+
+   //for(int i = 1; i<=10; i++)
+   //System.out.println("12 * " + i + " = " + (i * 12));
+
+    //for(int i = 1; i<=20; i ++)
+   //System.out.println(i + " -> " + i * i );
+    */
+   
+
+    }
+}
