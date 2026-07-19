90fd6c74f80a9f63d1a7a4f64384c271b942bffa
Rongala Kuladeep Manikumar <manikumarrongala11@gmail.com>
2026-07-19 23:57:27 +0530

    Added some new problems

--- NextedLoopsUsingPatteren.java
+++ NextedLoopsUsingPatteren.java
@@ -39,16 +39,29 @@ for(int i = 1; i<=n; i++){
 
     System.out.println();
     
-} */
+} 
 int n = 5;
 for(int i = 1; i<=n; i++){
     for(int j = 1; j<=i; j++)
         System.out.print(i);
 
         System.out.println();
-    }
+    } 
+        // for print A AB ABC ABCD ABCDE
+int n = 5;
+for(int i = 1; i<=n; i++){
+    for(int j = 1; j<=i; j++)
+        System.out.print((char)('A' + j -1));
+    System.out.println();
+}*/
+//ABEDE ABCD ABC AB A
+int n = 5;
+for(int i = 1; i<=n; i++){
+    for(int j = 1; j<=n - i +1; j++)
+        System.out.print((char)('A' + j -1));
+    System.out.println();
 }
 
-
+}
     }
 
