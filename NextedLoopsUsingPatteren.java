23b88950f0be0143e2a1c40094a9cc9648aee87a
Rongala Kuladeep Manikumar <manikumarrongala11@gmail.com>
2026-07-19 23:29:04 +0530

    Added new problems

--- NextedLoopsUsingPatteren.java
+++ NextedLoopsUsingPatteren.java
@@ -1,6 +1,54 @@
-public class NextedLoopsUsingPatteren {
+public class NextedLoopsUsingPatteren{
     public static void main(String[] args){
-        
-    }
+/* 
+        int n = 4;
+        int m = 5;
+
+        for(int i = 1; i<=n; i++){
+            //inner Loop
+            for(int j = 2; j<=m; j++ ){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        } 
+       // 2. Print the rectangle
+
+       int n = 5;
+       int m = 5;
+
+       for(int i = 1; i<=n; i++){
+        for(int j =1; j<m; j++ ){
+            System.out.print("*");
+        }
+        System.out.println("*");
+       } 
+      /* int n = 5;
+      for(int i = 1; i<=n; i++){
+
+        for(int j = 1; j<=i; j++)
+
+        System.out.print("*");
+      
+System.out.println();
+} 
+
+int n = 5;
+for(int i = 1; i<=n; i++){
+    for(int j = 1;j<= n -i +1; j++)
+        System.out.print("*");
+
+    System.out.println();
     
+} */
+int n = 5;
+for(int i = 1; i<=n; i++){
+    for(int j = 1; j<=i; j++)
+        System.out.print(i);
+
+        System.out.println();
+    }
 }
+
+
+    }
+
