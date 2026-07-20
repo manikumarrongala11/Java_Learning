3b1ae43785251dc9cf00f4b541dff4b48d565a6d
Rongala Kuladeep Manikumar <manikumarrongala11@gmail.com>
2026-07-20 23:17:24 +0530

    Added Butterfly Sum

--- NextedLoopsUsingPatteren.java
+++ NextedLoopsUsingPatteren.java
@@ -218,6 +218,53 @@ for(int i = 2; i <=n; i++){
     System.out.println();
 }
 */
-}
+//butter fly pattern
+
+
+        int n = 4;
+
+        // Upper Half
+        for (int i = 1; i <= n; i++) {
+
+            // Left Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower Half
+        for (int i = n; i >= 1; i--) {
+
+            // Left Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
     }
+}
+
 
