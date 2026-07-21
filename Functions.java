e83ad189309f6e43cd051914b4f8bd96fc8f74aa
Rongala Kuladeep Manikumar <manikumarrongala11@gmail.com>
2026-07-21 07:40:51 +0530

    Functions Practice

---
+++ Functions.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Functions {
+    //Block of code which performs the specific code
+    //returnType functionName(type arg1, type arg2..){
+    //operations
+    //public static returnType functionName(parameters) {
+    // code
+}
+    // 1. Print a given name in a function
+
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter you name : ");
+        String name = sc.nextLine();
+        
+        printMyName(name);
+    
+    }
+    
+}
